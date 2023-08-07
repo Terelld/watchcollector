e1a082611049d3d7d9e91b3bdff65b421acf0113
Terelld <terelld@gmail.com>
2023-08-07 10:09:35 -0500

    renders an index page that dynamically displays a hardcoded list of watches

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+watches = [
+    {'brand': 'Fossil', 'model':'', 'band material': 'metal', 'color': 'gun metal', 'interface': 'digital', 'description': 'casual'},
+    {'brand': 'Rolex', 'model':'', 'band material': 'leather', 'color': 'brown', 'interface': 'analog', 'description': 'formal'},
+    {'brand': 'Nike', 'model':'', 'band material': 'silicone', 'color': 'black', 'interface': 'digital', 'description': 'athletic'},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def watches_index(request):
+    return render(request, 'watches/index.html')
